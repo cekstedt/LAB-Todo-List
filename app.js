4171f32ebf5032988170df08f5e569ee9bc9a69e
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-17 14:01:47 -0700

    Allow for creation of custom lists.

--- app.js
+++ app.js
@@ -16,12 +16,22 @@ mongoose.connect("mongodb://localhost:27017/todolistDB");
 // Global variables.
 
 const port = process.env.PORT;
+
 const itemsSchema = new mongoose.Schema({
   name: String
 });
 const Item = mongoose.model("Item", itemsSchema);
 
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema]
+});
+const List = mongoose.model("List", listSchema);
 
+const item1 = new Item({ name: "Welcome to your Todo List!" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+const defaultList = [item1, item2, item3];
 
 // Get routes.
 
@@ -31,10 +41,7 @@ app.get("/", function(req, res) {
       console.log(err);
     } else {
       if (foundItems.length === 0) {
-        const item1 = new Item({ name: "Welcome to your Todo List!" });
-        const item2 = new Item({ name: "Hit the + button to add a new item." });
-        const item3 = new Item({ name: "<-- Hit this to delete an item." });
-        foundItems = [item1, item2, item3];
+        foundItems = defaultList;
         Item.insertMany(foundItems, handleError);
       }
       res.render("list", {
@@ -45,15 +52,37 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    listItems: workItems
-  })
+// Bug fix: Causing duplicate data in database.
+app.get("/favicon.ico", (req, res) => {
+  res.sendStatus(404);
 });
 
 app.get("/about", function(req, res) {
   res.render("about");
+});
+
+app.get("/:listName", function(req, res) {
+  const listName = req.params.listName;
+
+  List.findOne({ name: listName }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        // Create a new list.
+        const list = new List({
+          name: listName,
+          items: defaultList
+        });
+        list.save();
+        res.redirect("/" + listName);
+      } else {
+        // Show an existing list.
+        res.render("list", {
+          listTitle: foundList.name,
+          listItems: foundList.items
+        });
+      }
+    }
+  })
 });
 
 // Post routes.
